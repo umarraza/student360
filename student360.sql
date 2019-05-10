509ba63ac4d7b87a9314c85db1e0284952f955fa
Umar Raza <umarraza@stackcru.com>
2019-05-10 12:48:30 +0500

    added sql file

--- student360.sql
+++ student360.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 09, 2019 at 10:07 AM
+-- Generation Time: May 10, 2019 at 09:47 AM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.2
 
@@ -51,11 +51,18 @@ INSERT INTO `approve_hostel_requests` (`id`, `approveStatus`, `hostelId`, `creat
 
 CREATE TABLE `hostels_registration-requests` (
   `id` int(10) NOT NULL,
-  `approvalStatus` tinyint(4) NOT NULL DEFAULT '0',
+  `verificationStatus` tinyint(4) NOT NULL DEFAULT '0',
   `hostelId` int(10) NOT NULL,
   `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `updatedAt` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `hostels_registration-requests`
+--
+
+INSERT INTO `hostels_registration-requests` (`id`, `verificationStatus`, `hostelId`, `createdAt`, `updatedAt`) VALUES
+(1, 0, 20, '2019-05-09 22:47:08', '2019-05-09 22:47:08');
 
 -- --------------------------------------------------------
 
@@ -77,9 +84,9 @@ CREATE TABLE `hostel_images` (
 --
 
 INSERT INTO `hostel_images` (`id`, `imageName`, `isThumbnail`, `hostelId`, `createdAt`, `updatedAt`) VALUES
-(1, 'image_1557041414.png', 1, 1, '2019-05-05 14:30:14', '2019-05-05 14:30:14'),
-(2, 'image_1557120758.png', 0, 2, '2019-05-06 00:32:38', '2019-05-06 00:32:38'),
-(3, 'image_1557120758.png', 0, 1, '2019-05-06 05:44:29', '2019-05-06 05:44:29'),
+(1, 'image_1557041414.png', 1, 9, '2019-05-05 14:30:14', '2019-05-05 14:30:14'),
+(2, 'image_1557120758.png', 1, 15, '2019-05-06 00:32:38', '2019-05-06 00:32:38'),
+(3, 'image_1557120758.png', 1, 20, '2019-05-06 05:44:29', '2019-05-06 05:44:29'),
 (4, 'image_1557122005.png', 0, 3, '2019-05-06 00:53:25', '2019-05-06 00:53:25'),
 (6, 'image_1557122005.png', 0, 3, '2019-05-06 05:58:47', '2019-05-06 05:58:47');
 
@@ -111,7 +118,7 @@ CREATE TABLE `hostel_profiles` (
   `isApproved` int(10) NOT NULL DEFAULT '0',
   `isVerified` int(10) NOT NULL DEFAULT '0',
   `isAvailable` tinyint(4) DEFAULT '0',
-  `features` varchar(1000) NOT NULL,
+  `features` varchar(2000) NOT NULL,
   `userId` int(10) NOT NULL,
   `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
@@ -122,9 +129,10 @@ CREATE TABLE `hostel_profiles` (
 --
 
 INSERT INTO `hostel_profiles` (`id`, `hostelName`, `hostelCategory`, `numberOfBedRooms`, `noOfBeds`, `priceRange`, `address`, `longitude`, `latitude`, `state`, `postCode`, `city`, `country`, `description`, `contactName`, `contactEmail`, `website`, `phoneNumber`, `isApproved`, `isVerified`, `isAvailable`, `features`, `userId`, `createdAt`, `updatedAt`) VALUES
-(9, 'MASHALLAH BOYS HOSTEL GULBERG 3 NEAR HAJVERY UNIVERSITY', 'Boys', 10, 20, '10000-15000', 'iqbal town', '31.520370', '74.358747', 'punjab', 54000, 'lahore', 'pakistan', 'Mashallah Boys Hostel Gulberg 3 near hajvery university Facilities: Wifi Internet - Free UPS Connection - Free TV cable Connection - Free Attach Bath Carpeted Room Geyser for Hot Water in winters Electric Water Cooler for Drinking -Electricity (separate sub meters installed) Room Sharing Rent 4000 (Security 5000) Full Room Rent 8000 (Security 10000) Max 2 persons allowed in a room.', 'anas', 'abc@gmail.com', 'abc.com', '03249470780', 1, 0, 0, '[\"ATM\",\"BBQ area\"]', 39, '2019-05-07 21:44:54', '2019-05-09 02:47:19'),
-(15, 'Fast Hostel', 'Girls', 25, 15, '5000', '370,TIP BLOCK KHAYABAN-E-AMIN,DEFENCE ROAD LAHORE 370 TIP BLOCK KHAYABAN E AMEEN LAHORE', '74.358747', '31.520370', 'Punjab', 212121, 'Lahore', 'Pakistan', 'Mairona Hotels Gulberg is located in Lahore, 28 km from Wagah Border, and offers free WiFi. Located around 1.2 km from Pace Shopping Mall, the hotel is also 2.1 km away from Lahore Gymkhana. A tour desk can provide information on the area.', 'Uzair', 'uzairsalar@gmail.com', 'www.maironahotel.com', '03218840489', 0, 0, 0, '[\"ATM\",\"BBQ area\"]', 49, '2019-05-08 20:30:14', '2019-05-08 20:30:14'),
-(17, 'Continental Hostel', 'Girls', 25, 15, '5000', '370,TIP BLOCK KHAYABAN-E-AMIN,DEFENCE ROAD LAHORE 370 TIP BLOCK KHAYABAN E AMEEN LAHORE', '74.358747', '31.520370', 'Punjab', 212121, 'Lahore', 'Pakistan', 'Mairona Hotels Gulberg is located in Lahore, 28 km from Wagah Border, and offers free WiFi. Located around 1.2 km from Pace Shopping Mall, the hotel is also 2.1 km away from Lahore Gymkhana. A tour desk can provide information on the area.', 'Umar Raza', 'umarraza@gmail.com', 'www.maironahotel.com', '03218840489', 0, 0, 0, '[\"ATM\",\"BBQ area\"]', 52, '2019-05-09 03:04:45', '2019-05-09 03:04:45');
+(9, 'MASHALLAH BOYS HOSTEL GULBERG 3 NEAR HAJVERY UNIVERSITY', 'Boys', 10, 20, '10000-15000', 'iqbal town', '31.520370', '74.358747', 'punjab', 54000, 'lahore', 'pakistan', 'Mashallah Boys Hostel Gulberg 3 near hajvery university Facilities: Wifi Internet - Free UPS Connection - Free TV cable Connection - Free Attach Bath Carpeted Room Geyser for Hot Water in winters Electric Water Cooler for Drinking -Electricity (separate sub meters installed) Room Sharing Rent 4000 (Security 5000) Full Room Rent 8000 (Security 10000) Max 2 persons allowed in a room.', 'anas', 'abc@gmail.com', 'abc.com', '03249470780', 1, 0, 0, '[\"ATM\",\"waterFilter\"]', 39, '2019-05-07 21:44:54', '2019-05-09 02:47:19'),
+(15, 'Fast Hostel', 'Girls', 25, 15, '10000-15000', '370,TIP BLOCK KHAYABAN-E-AMIN,DEFENCE ROAD LAHORE 370 TIP BLOCK KHAYABAN E AMEEN LAHORE', '74.358747', '31.520370', 'Punjab', 212121, 'Lahore', 'Pakistan', 'Mairona Hotels Gulberg is located in Lahore, 28 km from Wagah Border, and offers free WiFi. Located around 1.2 km from Pace Shopping Mall, the hotel is also 2.1 km away from Lahore Gymkhana. A tour desk can provide information on the area.', 'Uzair', 'uzairsalar@gmail.com', 'www.maironahotel.com', '03218840489', 0, 0, 0, '[\"ATM\",\"attachedBathroom\",\"seprate\"]', 49, '2019-05-08 20:30:14', '2019-05-08 20:30:14'),
+(17, 'Continental Hostel', 'Girls', 25, 15, '5000', '370,TIP BLOCK KHAYABAN-E-AMIN,DEFENCE ROAD LAHORE 370 TIP BLOCK KHAYABAN E AMEEN LAHORE', '74.358747', '31.520370', 'Punjab', 212121, 'Lahore', 'Pakistan', 'Mairona Hotels Gulberg is located in Lahore, 28 km from Wagah Border, and offers free WiFi. Located around 1.2 km from Pace Shopping Mall, the hotel is also 2.1 km away from Lahore Gymkhana. A tour desk can provide information on the area.', 'Umar Raza', 'umarraza@gmail.com', 'www.maironahotel.com', '03218840489', 0, 0, 0, '[\"ATM\",\"attachedBathroom\",\"seprate\"]', 52, '2019-05-09 03:04:45', '2019-05-09 03:04:45'),
+(20, 'Friends Hostel', 'Boys', 20, 30, '10000-12000', '280 D block, Faisal town lahore', '31.520370', '74.358747', 'punjab', 54000, 'Lahore', 'Pakistan', 'hostel is at good location with all the fascilities.', 'Rehan', 'Rehan@gmail.com', 'friends.com', '03211234567', 0, 0, 0, '[\"24-hour reception\",\"24-hour security\",\"Air conditioning\",\"ATM\",\"BBQ area\",\"Parking area\",\"Board Games\",\"Mess\",\"TV\",\"Ceiling Fan\",\"Childrens play area\",\"Common Room\",\"Currency Exchange\",\"Direct Dial Telephone\",\"Kitchen\",\"Elevator\",\"Free Internet Access\",\"Free Parking\",\"Free WiFi\",\"Fridge/Freezer\",\"Fusball\",\"Games Room\",\"Gym\",\"Housekeeping\",\"Internet Access\",\"Iron/ironing Board\",\"Laundry Facilities\",\"Lockers\",\"Luggage Storage\",\"Microwave\",\"Outdoor Swimming Pool\",\"Outdoor Terrace\",\"Pool Table\",\"Reception (limited hours)\",\"Swimming Pool\",\"Tea & Coffee Making Facilities\",\"Vending Machines\",\"Wake-up calls\",\"Washing Machine\",\"Wheelchair Accessible\",\"Video games\",\"Transport\",\"CCTV\",\"Sports area\",\"Lawn\",\"Attached bathrooms\",\"Room service\",\"Pets allowed\",\"Guests allowed\",\"Water filter\",\"First aid\",\"Furnished\",\"Not furnished\",\"UPS\",\"Geyser\",\"Study room\",\"Medical support\",\"Ground Floor\",\"Professional cook\",\"Carpeted rooms\",\"Cupboards\",\"Daily news paper\",\"Electricity 24/7\",\"3-time meal\",\"Non- Smoking\",\"seprate\"]', 57, '2019-05-09 22:47:08', '2019-05-09 22:47:08');
 
 -- --------------------------------------------------------
 
@@ -219,9 +227,9 @@ CREATE TABLE `ratings` (
 --
 
 INSERT INTO `ratings` (`id`, `score`, `userId`, `hostelId`, `createdAt`, `updatedAt`) VALUES
-(1, 'Very good hostel', 15, 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(2, 'Quite affordable prices, good facilities', 16, 2, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(3, 'Amazing facilities', 18, 3, '2019-05-06 05:54:08', '2019-05-06 05:54:08');
+(1, '1', 15, 20, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(2, '2', 16, 9, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(3, '3', 18, 15, '2019-05-06 05:54:08', '2019-05-06 05:54:08');
 
 -- --------------------------------------------------------
 
@@ -369,7 +377,8 @@ INSERT INTO `users` (`id`, `username`, `email`, `password`, `remember_token`, `r
 (39, 'l1f14bscs0068', NULL, '$2y$10$qQ4eksvopVasJatY2OzaZul9KpkpIA.YqmPBGKn3e6mpUOmVOMXby', NULL, 2, NULL, NULL, NULL, NULL, 0, 0, NULL, NULL, 'English', 0, '2019-05-07 21:44:54', '2019-05-07 21:44:54'),
 (49, 'uzairsalar@gmail.com', 'uzairsalar@gmail.com', '$2y$10$w5J5dS6PdH3vsOLNRndpcOFZH7pUBqnkdQco0QdPlN8Pey7SPW/Ai', NULL, 2, NULL, NULL, NULL, NULL, 0, 1, NULL, NULL, 'English', 3, '2019-05-09 05:54:22', '2019-05-08 20:30:14'),
 (50, 'usman@gmail.com', NULL, '$2y$10$90jzB05LV4yLB4nVxbbHYOv249xHfzonyiSDeiOgRxLpAL6rBruLe', NULL, 3, NULL, NULL, NULL, NULL, 0, 1, NULL, NULL, 'English', 6, '2019-05-09 03:34:09', '2019-05-08 22:34:09'),
-(52, 'umarraza@gmail.com', 'umarraza@gmail.com', '$2y$10$OXOrevcQvUsO38t3douf6O.JehyC3TLCH1BVCKxLxblTE5H5XpFF.', NULL, 2, NULL, NULL, NULL, NULL, 0, 0, NULL, NULL, 'English', NULL, '2019-05-09 03:04:45', '2019-05-09 03:04:45');
+(52, 'umarraza@gmail.com', 'umarraza@gmail.com', '$2y$10$OXOrevcQvUsO38t3douf6O.JehyC3TLCH1BVCKxLxblTE5H5XpFF.', NULL, 2, NULL, NULL, NULL, NULL, 0, 0, NULL, NULL, 'English', NULL, '2019-05-09 03:04:45', '2019-05-09 03:04:45'),
+(57, 'rehan.rehan', 'Rehan@gmail.com', '$2y$10$9Z.NhqP63wq6jpl8qxLgQO68tqYPnsItrrFlwqmbq2Af1Y4QLrG6W', NULL, 2, NULL, NULL, NULL, NULL, 0, 1, NULL, NULL, 'English', NULL, '2019-05-10 04:37:18', '2019-05-09 22:47:08');
 
 -- --------------------------------------------------------
 
@@ -388,7 +397,7 @@ CREATE TABLE `user_profiles` (
   `institute` varchar(20) DEFAULT NULL,
   `dateOfBirth` varchar(50) DEFAULT NULL,
   `gender` varchar(20) DEFAULT NULL,
-  `CNIC` int(50) DEFAULT NULL,
+  `CNIC` varchar(50) DEFAULT NULL,
   `isVerified` int(10) NOT NULL DEFAULT '0',
   `userId` int(10) NOT NULL,
   `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -400,7 +409,7 @@ CREATE TABLE `user_profiles` (
 --
 
 INSERT INTO `user_profiles` (`id`, `fullName`, `phoneNumber`, `email`, `city`, `country`, `occupation`, `institute`, `dateOfBirth`, `gender`, `CNIC`, `isVerified`, `userId`, `createdAt`, `updatedAt`) VALUES
-(1, 'anas', '03249470780', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 34, '2019-05-07 00:05:00', '2019-05-07 00:05:00'),
+(1, 'Umar Raza', '03249470780', 'umarraza@gmail.com', 'Lahore', 'Pakistan', 'Student', 'Virtual University', '15-03-1995', 'Male', '35201-123456-7', 0, 34, '2019-05-07 00:05:00', '2019-05-07 00:05:00'),
 (2, 'Usman Munir', '03034969407', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 50, '2019-05-08 22:34:09', '2019-05-08 22:34:09');
 
 --
@@ -506,7 +515,7 @@ ALTER TABLE `approve_hostel_requests`
 -- AUTO_INCREMENT for table `hostels_registration-requests`
 --
 ALTER TABLE `hostels_registration-requests`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `hostel_images`
@@ -518,7 +527,7 @@ ALTER TABLE `hostel_images`
 -- AUTO_INCREMENT for table `hostel_profiles`
 --
 ALTER TABLE `hostel_profiles`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `mess_menu`
@@ -536,7 +545,7 @@ ALTER TABLE `profile_pictures`
 -- AUTO_INCREMENT for table `queries`
 --
 ALTER TABLE `queries`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `ratings`
@@ -572,7 +581,7 @@ ALTER TABLE `update-hostels-requests`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
 
 --
 -- AUTO_INCREMENT for table `user_profiles`
