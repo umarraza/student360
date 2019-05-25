cc54a8d69db64b35a8dd55eb7ec8184713009925
Umar Raza <umarraza@stackcru.com>
2019-05-25 12:23:37 +0500

    added database transactions protetions

--- app/Http/Controllers/Api/HostelController.php
+++ app/Http/Controllers/Api/HostelController.php
@@ -21,6 +21,7 @@ use App\Models\Api\ApiMessMenuMeal as MessMenuMeal;
 use App\Models\Api\ApiMessMenuTiming as MessMenuTiming;
 use App\Models\Api\ApiUpdateHostelRequest as UpdateHostelRequest;
 use App\Models\Api\ApiVerifyHostelRequests as VerifyHostelRequests;
+use Exception;
 
 
 class HostelController extends Controller
@@ -97,42 +98,86 @@ class HostelController extends Controller
                 $roleId   = $request->get('roleId');
                 $email    = $request->get('contactEmail');
 
-                $user =  User::create([
-                    
-                    'username'   =>  $username,
-                    'email'      =>  $email,
-                    'password'   =>  bcrypt($password),
-                    'roleId'     =>  $roleId,
-                    'verified'   =>  0,
-                    'language'   =>  "English",
-                ]);
+                DB::beginTransaction();
+
+                try{
+                    $user =  User::create([
+                        
+                        'username'   =>  $username,
+                        'email'      =>  $email,
+                        'password'   =>  bcrypt($password),
+                        'roleId'     =>  $roleId,
+                        'verified'   =>  0,
+                        'language'   =>  "English",
+                    ]);
+
+                    if ($user->save()) {
+
+                        DB::commit();
+                        $response['data']['code']           = 200;
+                        $response['status']                 = true;
+                        $response['data']['user']           = $user;
+                        $response['data']['message']        = 'User created Successfully';
+
+                    } else {
+
+                        DB::rollBack();
+
+                    }
+                } catch (Exception $e) {
+
+                    DB::rollBack();
+
+                }
 
                 $userId = $user->id;
 
-                $hostel = Hostel::create([
+                DB::beginTransaction();
 
-                        'hostelName'       =>   $request->get('hostelName'),
-                        'hostelCategory'   =>   $request->get('hostelCategory'), 
-                        'numberOfBedRooms' =>   $request->get('numberOfBedRooms'),
-                        'noOfBeds'         =>   $request->get('noOfBeds'),
-                        'priceRange'       =>   $request->get('priceRange'),
-                        'address'          =>   $request->get('address'),
-                        'longitude'        =>   $request->get('longitude'),
-                        'latitude'         =>   $request->get('latitude'),
-                        'state'            =>   $request->get('state'),
-                        'postCode'         =>   $request->get('postCode'),
-                        'city'             =>   $request->get('city'),
-                        'country'          =>   $request->get('country'),
-                        'description'      =>   $request->get('description'),
-                        'contactName'      =>   $request->get('contactName'),
-                        'contactEmail'     =>   $request->get('contactEmail'),
-                        'website'          =>   $request->get('website'),
-                        'phoneNumber'      =>   $request->get('phoneNumber'),
-                        'features'         =>   $request->get('features'),
-                        'isVerified'       =>   0,
-                        'userId'           =>   $user->id,
+                try{
 
-                    ]);
+                    $hostel = Hostel::create([
+
+                            'hostelName'       =>   $request->get('hostelName'),
+                            'hostelCategory'   =>   $request->get('hostelCategory'), 
+                            'numberOfBedRooms' =>   $request->get('numberOfBedRooms'),
+                            'noOfBeds'         =>   $request->get('noOfBeds'),
+                            'priceRange'       =>   $request->get('priceRange'),
+                            'address'          =>   $request->get('address'),
+                            'longitude'        =>   $request->get('longitude'),
+                            'latitude'         =>   $request->get('latitude'),
+                            'state'            =>   $request->get('state'),
+                            'postCode'         =>   $request->get('postCode'),
+                            'city'             =>   $request->get('city'),
+                            'country'          =>   $request->get('country'),
+                            'description'      =>   $request->get('description'),
+                            'contactName'      =>   $request->get('contactName'),
+                            'contactEmail'     =>   $request->get('contactEmail'),
+                            'website'          =>   $request->get('website'),
+                            'phoneNumber'      =>   $request->get('phoneNumber'),
+                            'features'         =>   $request->get('features'),
+                            'isVerified'       =>   0,
+                            'userId'           =>   $user->id,
+
+                        ]);
+
+                        if ($hostel->save()) {
+                            
+                            DB::commit();
+
+                            $response['data']['code']           = 200;
+                            $response['status']                 = true;
+                            $response['data']['user']           = $hostel;
+                            $response['data']['message']        = 'Hostel created Successfully';
+
+                        } else {
+                            
+                            DB::rollBack();
+                        }
+                    } catch (Exception $e) {
+
+                        DB::rollBack();
+                    }
 
                 $hostelId  = $hostel->id;
 
@@ -143,24 +188,41 @@ class HostelController extends Controller
                  *  @return Model
                  */
 
-                for ($i = 0; $i < 6; ++$i){
-                    
-                    $days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+                DB::beginTransaction();
 
-                    $messMenueMeal = MessMenuMeal::create([
+                try{
 
-                        'day'            =>  $days[$i],
-                        'breakFastMeal'  =>  'Set Break Fast Meal',
-                        'LunchMeal'      =>  'Set lunch Meal',
-                        'dinnerMeal'     =>  'Set Dinner Meal',
-                        'hostelId'       =>  $hostelId,
-    
-                    ]);
+                    for ($i = 0; $i < 6; ++$i){
+                        
+                        $days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-                    $messMenueMeal->save();
+                        $messMenueMeal = MessMenuMeal::create([
+
+                            'day'            =>  $days[$i],
+                            'breakFastMeal'  =>  'Set Break Fast Meal',
+                            'LunchMeal'      =>  'Set lunch Meal',
+                            'dinnerMeal'     =>  'Set Dinner Meal',
+                            'hostelId'       =>  $hostelId,
+        
+                        ]);
+
+                        if ($messMenueMeal->save()) {
+
+                            DB::commit();
+                            $response['data']['code']           = 200;
+                            $response['status']                 = true;
+                            $response['data']['user']           = $messMenueMeal;
+                            $response['data']['message']        = 'Mess Menu Meal created Successfully';
+                        
+                        }else{
+                            DB::rollback();
+                        }
+                    }
+                } catch (Exception $e) {
+
+                    DB::rollBack();
 
                 }
-
 
                 /**
                  * 
@@ -173,29 +235,44 @@ class HostelController extends Controller
                  * 
                  */
 
-                $messMenuTiming = MessMenuTiming::create([
+                DB::beginTransaction();
 
-                    'brkfastStartTime'  =>  '07:00 AM ',
-                    'brkfastEndTime'    =>  '10:00 AM',
-                    'lunchStartTime'   =>  '01:00 PM',
-                    'lunchEndTime'     =>  '03:00 PM',
-                    'dinnerStartTime'  =>  '07:00 PM',
-                    'dinnerEndTime'    =>  '10:00 PM',
-                    'isSetBreakFast'   =>  0,
-                    'isSetLunch'       =>  0,
-                    'isSetDinner'      =>  0,
-                    'hostelId'         =>  $hostelId,
+                try {
 
-                ]);
+                    $messMenuTiming = MessMenuTiming::create([
 
-            	if ($hostel->save() && $user->save()&& $messMenuTiming->save()) 
-                {
-                    $response['data']['code']           = 200;
-                    $response['status']                 = true;
-                    $response['data']['result']         = $hostel;
-                    $response['data']['user']           = $user;
-                    $response['data']['messMenuTiming'] = $messMenuTiming;
-                    $response['data']['message']        = 'Hostel created Successfully';
+                        'brkfastStartTime'  =>  '07:00 AM ',
+                        'brkfastEndTime'    =>  '10:00 AM',
+                        'lunchStartTime'    =>  '01:00 PM',
+                        'lunchEndTime'      =>  '03:00 PM',
+                        'dinnerStartTime'   =>  '07:00 PM',
+                        'dinnerEndTime'     =>  '10:00 PM',
+                        'isSetBreakFast'    =>  0,
+                        'isSetLunch'        =>  0,
+                        'isSetDinner'       =>  0,
+                        'hostelId'          =>  $hostelId,
+
+                    ]);
+
+                    if ($messMenuTiming->save()) {
+
+                        DB::commit();
+                        $response['data']['code']          =  200;
+                        $response['status']                =  true;
+                        $response['data']['user']          =  $user;
+                        $response['data']['hostel']        =  $hostel;
+                        $response['data']['messMenuMeal']  =  $messMenueMeal;
+                        $response['data']['messMenuTime']  =  $messMenuTiming;
+                        $response['data']['message']       = 'Hostel created Successfully';
+                        
+                    } else {
+
+                        DB::rollBack();
+                    }
+
+                } catch (Exception $e) {
+
+                    DB::rollBack();
                 }
             }
         
@@ -234,20 +311,30 @@ class HostelController extends Controller
                'status' => false
             ];
             
-            $allHostels = Hostel::select('id','hostelName',  'address', 'hostelCategory', 'isVerified', 'isApproved')->get();
+            DB::beginTransaction();
 
-            if (!empty($allHostels)) {
+            try {
 
-                $response['data']['code']       =  200;
-                $response['data']['message']    =  'Request Successfull';
-                $response['data']['result']     =  $allHostels;
-                $response['status']             =  true;
+                $allHostels = Hostel::select('id','hostelName',  'address', 'hostelCategory', 'isVerified', 'isApproved')->get();
 
-            } else {
+                if (!empty($allHostels)) {
+                    
+                    $response['data']['code']       =  200;
+                    $response['data']['message']    =  'Request Successfull';
+                    $response['data']['result']     =  $allHostels;
+                    $response['status']             =  true;
 
-                $response['data']['code']       =  400;
-                $response['data']['message']    =  'No Hostels Found';
-                $response['status']             =  false;    
+                } else {
+
+                    $response['data']['code']       =  400;
+                    $response['data']['message']    =  'No Hostels Found';
+                    $response['status']             =  false;    
+                }
+
+            } catch (Exception $e) {
+
+                DB::rollBack();
+
             }
         }
         return $response;
@@ -265,6 +352,7 @@ class HostelController extends Controller
 
     public function listRegisteredHostels(Request $request)
     {
+
         $user = JWTAuth::toUser($request->token);
         $response = [
                 'data' => [
@@ -285,21 +373,30 @@ class HostelController extends Controller
                'status' => false
             ];
             
-            $registeredHostels = Hostel::where('isVerified', 1)->get();
+            DB::beginTransaction();
 
-            if (!empty($registeredHostels)) {
+            try{
 
-                $response['data']['code']       =  200;
-                $response['data']['message']    =  'Request Successfull';
-                $response['data']['result']     =  $registeredHostels;
-                $response['status']             =  true;
+                $registeredHostels = Hostel::where('isVerified', 1)->get();
 
-            } else {
+                if (!empty($registeredHostels)) {
 
-                $response['data']['code']       =  400;
-                $response['data']['message']    =  'No Hostels Found';
-                $response['status']             =  false;    
-            }
+                    $response['data']['code']       =  200;
+                    $response['data']['message']    =  'Request Successfull';
+                    $response['data']['result']     =  $registeredHostels;
+                    $response['status']             =  true;
+
+                } else {
+
+                    $response['data']['code']       =  400;
+                    $response['data']['message']    =  'No Hostels Found';
+                    $response['status']             =  false;    
+                }
+
+            } catch (Exception $e) {
+
+                DB::rollBack();
+            }       
         }
         return $response;
     }
@@ -349,30 +446,36 @@ class HostelController extends Controller
             else
             {
 
-                $hostel = Hostel::find($request->id);
+                DB::beginTransaction();
 
-                $hostelImages = Image::where('hostelId', '=', $request->id)->get();
+                try {
 
-                $reviews = Review::where('hostelId', '=', $request->id)->get();
+                    $hostel = Hostel::find($request->id);
 
-                $ratings = Rating::where('hostelId', '=', $request->id)->get();
+                    $hostelImages   =  Image::where('hostelId', '=', $request->id)->get();
+                    $reviews        =  Review::where('hostelId', '=', $request->id)->get();
+                    $ratings        =  Rating::where('hostelId', '=', $request->id)->get();
 
-                if (!empty($hostel)) 
-                {
-                    $response['data']['code']                     =  200;
-                    $response['status']                           =  true;
-                    $response['data']['result']['hostelDetails']  =  $hostel;
-                    $response['data']['result']['images']         =  $hostelImages;
-                    $response['data']['result']['reviews']        =  $reviews;
-                    $response['data']['result']['ratings']        =  $ratings;
-                    $response['data']['message']                  =  'Request Successfull';
+                    if (!empty($hostel)) 
+                    {
+                        $response['data']['code']                     =  200;
+                        $response['status']                           =  true;
+                        $response['data']['result']['hostelDetails']  =  $hostel;
+                        $response['data']['result']['images']         =  $hostelImages;
+                        $response['data']['result']['reviews']        =  $reviews;
+                        $response['data']['result']['ratings']        =  $ratings;
+                        $response['data']['message']                  =  'Request Successfull';
 
-                } else {
+                    } else {
 
-                    $response['data']['code']       =  400;
-                    $response['status']             =  false;
-                    $response['data']['message']    =  'Hostel Not Found!';
-                
+                        $response['data']['code']       =  400;
+                        $response['status']             =  false;
+                        $response['data']['message']    =  'Hostel Not Found!';
+                    
+                    }
+                } catch (Exception $e) {
+
+                    DB::rollBack();
                 }
             }
         return $response;
@@ -423,27 +526,50 @@ class HostelController extends Controller
 
                 $hostel = Hostel::find($request->id);
                 $userId = $hostel->userId;
+                $hostelId = $hostel->id;
 
                 $user = User::find($userId);
+
+                $messMenuMeal = MessMenuMeal::where('hostelId', '=', $hostelId)->get();
+                $messMenuTiming = MessMenuTiming::where('hostelId', '=', $hostelId)->first();
+
+                foreach ($messMenuMeal as $value) {
+                   
+                   $value->delete();
                 
-            	if ($user->delete() && $hostel->delete()) {
+                }
 
-                    $response['data']['code']       = 200;
-                    $response['status']             = true;
-                    $response['data']['message']    = 'Hostel Deleted Successfully';
-
-                } else {
-
-                    $response['data']['code']       = 400;
-                    $response['status']             = false;
-                    $response['data']['message']    = 'Hostel Not Deleted!';
+                $array = [$user, $hostel, $messMenuTiming];
                 
+                DB::beginTransaction();
+
+                try{
+
+                    for ($i=0; $i < count($array); $i++) { 
+
+                        if ($array[$i]->delete()) {
+                        
+                            DB::commit();
+                            $response['data']['code']       = 200;
+                            $response['status']             = true;
+                            $response['data']['message']    = 'Hostel Deleted Successfully';
+    
+                        } else {
+    
+                            DB::rollBack();
+                            $response['data']['code']       = 400;
+                            $response['status']             = false;
+                            $response['data']['message']    = 'Hostel Not Deleted!';
+                        
+                        }
+                    }
+
+                } catch (Exception $e){
+    
+                    DB::rollBack();
                 }
             }
         
         return $response;
     }
-
-    
-
 }
